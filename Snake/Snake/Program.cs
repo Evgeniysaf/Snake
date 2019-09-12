a5cc15a53e285875ea16855dfe3ac5356b582554
saf <e-safonov@bk.ru>
2019-09-12 17:57:49 +0300

    12 09

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -10,7 +10,31 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Helo");
+            int x1 = 6;
+            int y1 = 3;
+            char sym1 = '*';
+
+            Console.SetCursorPosition(x1, y1);
+            Console.Write( sym1 );
+
+
+            int x2 = 4;
+            int y2 = 5;
+            char sym2 = '#';
+
+            Console.SetCursorPosition(x2, y2);
+            Console.Write( sym2 );
+
+
+            int x3 = 6;
+            int y3 = 7;
+            char sym3 = '@';
+
+            Console.SetCursorPosition(x3, y3);
+            Console.Write(sym3);
+
+
+
             Console.ReadLine();
   
         }
