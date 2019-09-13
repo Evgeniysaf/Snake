40b24582b196c9fc92ea1750f7a05930082b8214
saf <e-safonov@bk.ru>
2019-09-13 14:07:57 +0300

    Добавлена функция Drow

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -14,29 +14,22 @@ namespace Snake
             int y1 = 3;
             char sym1 = '*';
 
-            Console.SetCursorPosition(x1, y1);
-            Console.Write( sym1 );
-
+            Drow(x1, y1, sym1);
 
             int x2 = 4;
             int y2 = 5;
             char sym2 = '#';
 
-            Console.SetCursorPosition(x2, y2);
-            Console.Write( sym2 );
-
-
-            int x3 = 6;
-            int y3 = 7;
-            char sym3 = '@';
-
-            Console.SetCursorPosition(x3, y3);
-            Console.Write(sym3);
-
+            Drow(x2, y2, sym2);
 
 
             Console.ReadLine();
-  
+        }
+
+        static void Drow(int x, int y,char sym)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
         }
     }
 }
