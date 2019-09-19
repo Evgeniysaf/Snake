a4b44d5561f3cc86b380d7ae44a2e86b38ed581c
saf <e-safonov@bk.ru>
2019-09-19 19:20:38 +0300

    Lesson6

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -20,9 +20,53 @@ namespace Snake
 
             p2.Drow();
 
-            Point pp22 = new Point(6, 12, '$');
 
-            pp22.Drow();
+            Point p3 = new Point(6, 7, '@');
+
+            p3.Drow();
+
+            Point p4 = new Point(3, 2, '%');
+
+            p4.Drow();
+
+            Point p5 = new Point(5, 6, '&');
+
+            p5.Drow();
+
+
+
+
+
+
+            List<int> numList = new List<int>();
+            numList.Add(0);
+            numList.Add(1);
+            numList.Add(2);
+            numList.Add(3);
+            numList.Add(4);
+
+            int x = numList[0];
+            int y = numList[1];
+            int z = numList[2];
+            int a = numList[3];
+            int b = numList[4];
+
+            foreach(int i in numList)
+            {
+                Console.WriteLine(i);
+
+            }
+
+            List<Point> Plist = new List<Point>();
+            Plist.Add(p1);
+            Plist.Add(p2);
+
+            List<Point> dzList = new List<Point>();
+            dzList.Add(p3);
+            dzList.Add(p4);
+            dzList.Add(p5);
+      
+
 
             Console.ReadLine();
         }
